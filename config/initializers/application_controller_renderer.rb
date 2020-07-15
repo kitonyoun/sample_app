b355044b215908eb0b3f8322ce8179943d06149f
宮田 知弥 <miyatatomoya@miyatanoMacBook-Air.local>
2020-07-15 19:27:09 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eed912bd091cc8482cfeac00ff16c4c2991d0888f92697038367cc0d40631155bfd58d27f536919b254b112be02214ef256abc9fe3c26b810fe02d137ff1ba95'
