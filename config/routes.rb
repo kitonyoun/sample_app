515f2227a0e959b5d01ac6b253671a500d0e8073
宮田 知弥 <miyatatomoya@miyatanoMacBook-Air.local>
2020-07-16 14:47:10 +0900

    [finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # .../todolists/1や .../todolists/3 に該当する
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
